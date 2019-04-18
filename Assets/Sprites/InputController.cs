d88587bbe6a98a20b67565addc460c8ef4c1838f
zxc101 <zxc101@mail.ru>
2019-04-19 01:27:43 +0300

    Реализовал первые наброски ходьбы главного персонажа

---
+++ Assets/Sprites/InputController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class InputController : MonoBehaviour, IFixedUpdate
+{
+    private PlayerController _playerController;
+    private CameraController _cameraController;
+
+    void Start()
+    {
+        FindObjectOfType<FixedUpdateController>()?.AddFixedUpdate(this);
+
+        if (FindObjectOfType<PlayerController>())
+        {
+            _playerController = FindObjectOfType<PlayerController>();
+        }
+        else
+        {
+            Debug.LogError("Didn't find PlayerController");
+        }
+
+        if (FindObjectOfType<CameraController>())
+        {
+            _cameraController = FindObjectOfType<CameraController>();
+        }
+        else
+        {
+            Debug.LogError("Didn't find PlayerController");
+        }
+    }
+
+    public void OnFixedUpdate()
+    {
+        _playerController?.Rotate();
+        _playerController?.Move(Input.GetAxis("Vertical") * Vector2.up +
+                                Input.GetAxis("Horizontal") * Vector2.right);
+        _cameraController?.Move();
+
+    }
+}
---
+++ Assets/Sprites/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _speedRotation;
+
+    private Vector2 _currentDirection = new Vector3(0, 1, 0);
+    
+    public void Rotate()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 playerPosition = _player.position;
+        _player.up = Vector2.Lerp(_player.forward, mousePosition - playerPosition, 0.1f);
+    }
+
+    public void Move(Vector2 direction)
+    {
+        _player.Translate(Time.deltaTime * direction * _speed, Space.World);
+    }
+}
